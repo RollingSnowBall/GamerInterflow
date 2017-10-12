8ab8e5825526acbedc89bbbf3f74ce6aa5325e00
wp <wangp@soulgame.com>
2017-10-12 09:53:40 +0800

    提交合并bug

---
+++ web/src/com/strongpoint/pageController/LoginController.java
@@ -0,0 +1,9 @@
+package com.strongpoint.pageController;
+
+import com.jfinal.core.Controller;
+
+public class LoginController extends Controller  {
+	public void index() {
+		render("login.html");
+	}
+}
